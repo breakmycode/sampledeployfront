8367fa4b963ba207651bda5e92643930f77155a8
breakmycode <vaibhavkargeti@gmail.com>
2019-04-12 09:54:42 +0530

    initial commit

---
+++ readmeDeploy.js
@@ -0,0 +1,53 @@
+/*  
+
+How to deploy a website ?
+(see img dlp1im)
+ans-- deploy could b either in fronmtend or backend 
+-- for the frontend deployment , we have static hosts and HTML hosting 
+-- for the frontend+backend or anywhere there is backend there 
+servers+cloud hosting+ VPS(virtual private service)  is required
+ 
+-- now , Backend hosting coulnt be done  for free vbeacuse it requires CPU+RAM+Disk+bandwidth
+whereas frontend deploying only requires bandwidth ... 
+maximum space it could use , it could be 10MB.as it is only HTML+JS+CSS code. 
+
+
+==> Bare metal 
+==> Infrastructure as a service (Iaas)
+types of Iaas services:
+AWS-- amazon web services
+GCE-- Google clouyd Engine 
+Digital ocean
+
+(see img Iaas2)
+PLATFORM AS A SERVICE::
+-- we are a virtual computer , the computer has Ubuntu+Node.js+npm installed in it
+u just provide us a Git repo , we will clone 
+-- then we will npm install
+-- our responsibility is just to manage the source code
+-- put it into git , the service providers , they put it into the git 
+-- they install and they run it .
+-- this type of service is known as Platform as a service(Paas)
+
+-- in Paas, all the things exits like Os and all 
+example : Heroku
+we will give the source code to heroku and it will run our code.
+
+-- on Github also , if u publish ur frontend code , github can host frontend for u 
+coding blocks is actually hosted on github only..
+-- if we goto hackerblocks.com or online.codingblocks.com, they are Iaas-- means they are bot servers on cloud 
+
+
+-- lets say we we start with frontend only website..
+-- create an html 
+
+
+
+
+
+
+
+
+
+
+*/
